60c7e48cdde944350420ed1ed031a38a66457685
unknown <adhityaprahma@gmail.com>
2025-03-03 14:02:39 +0700

    DasProg

---
+++ Split_n_join.py
@@ -0,0 +1,11 @@
+Sentence = "Penculikan,Pemerkosaan,PerEwean"
+words = Sentence.split(".")
+print(words)
+
+def clean_word(word):
+    return word.strip()
+
+cleaned_words = list(map(clean_word, words))
+print(cleaned_words)
+
+print(".".join(cleaned_words))
