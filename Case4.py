4f59c03ffe1c6443c4b18a3587b34eff03921634
unknown <adhityaprahma@gmail.com>
2025-03-03 14:23:47 +0700

    DasProg

---
+++ Case4.py
@@ -0,0 +1,8 @@
+sentence =  '''Tim Hukum KPK meminta penundaan sidang praperadilan jilid II Sekjen PDIP Hasto Kristiyanto terkait penetapan status tersangka. Kuasa hukum Hasto berharap penundaan itu bukan akal-akalan KPK untuk menyelesaikan berkas perkara.'''
+
+sentence_split = sentence.split(",")
+short_sentence = ' '.join(sentence_split[:6])
+
+
+print(sentence)
+
