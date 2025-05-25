372b39877743e49ee4c3d8ad07460c0da7c565f5
unknown <adhityaprahma@gmail.com>
2025-05-25 17:53:51 +0700

    DasProg

--- project_ecommerce/ecommerce/order.py
+++ project_ecommerce/ecommerce/order.py
@@ -1,6 +1,5 @@
-# ecommerce/order.py
-
 import uuid
 
 def generate_order_id():
     return str(uuid.uuid4())
+
--- project_ecommerce/ecommerce/pricing.py
+++ project_ecommerce/ecommerce/pricing.py
@@ -1,13 +1,12 @@
-# ecommerce/pricing.py
+def hitung_diskon(harga_awal, persentase_diskon):
+    return harga_awal * (persentase_diskon / 100)
 
-def hitung_diskon(harga, persen_diskon):
-    return harga * (persen_diskon / 100)
+def hitung_pajak(harga_setelah_diskon, Persentase_pajak):
+    return harga_setelah_diskon * (Persentase_pajak / 100)
 
-def hitung_pajak(harga, persen_pajak):
-    return harga * (persen_pajak / 100)
-
-def hitung_total(harga, persen_diskon, persen_pajak):
-    diskon = hitung_diskon(harga, persen_diskon)
-    harga_setelah_diskon = harga - diskon
-    pajak = hitung_pajak(harga_setelah_diskon, persen_pajak)
-    return harga_setelah_diskon + pajak
+def hitung_total(harga_awal, persentase_diskon, persentase_pajak):
+    diskon = hitung_diskon(harga_awal, persentase_diskon)
+    harga_setelah_diskon = harga_awal - diskon
+    pajak = hitung_pajak(harga_setelah_diskon, persentase_pajak)
+    total = harga_setelah_diskon + pajak
+    return total
